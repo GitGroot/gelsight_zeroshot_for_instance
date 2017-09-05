56cf95040b0a0b1ced62110b0d8dcbf9990ada46
GitGroot <576962786@qq.com>
2017-09-05 18:34:39 +0800

    20170905 18:34

---
+++ excel_to_array.py
@@ -0,0 +1,13 @@
+import xlrd
+import numpy as np
+
+# data = xlrd.open_workbook('/home/wangfeng/PysicalProperty_human.xlsx')
+# table = data.sheets()[0]
+# nrows = table.nrows
+# ncols = table.ncols
+# class_attribute = []
+# for i in xrange(1,nrows):
+#     rowValues= table.row_values(i)
+#     class_attribute.append(rowValues[1:])
+# np.save('class_attribute_matrix', class_attribute)
+print np.load('class_attribute_matrix.npy')
---
+++ videoData.py
@@ -0,0 +1,74 @@
+import os
+import cv2
+import numpy as np
+import random
+data_path = '/home/wangfeng/download/gelSight/fuse/data/gel_fold3_224'
+
+class_attribute_matrix = np.load('class_attribute_matrix.npy')
+
+# TODO notice that train_label and test_label must be incremental, and start from 0
+train_labels = range(80)
+test_labels = range(80,100)
+
+def get_train_attr_label():
+    ret = []
+    for label in train_labels:
+        ret.append(class_attribute_matrix[label])
+    return np.array(ret)
+
+
+def get_test_attr_label():
+    ret = []
+    for label in test_labels:
+        ret.append(class_attribute_matrix[label])
+    return np.array(ret)
+
+
+class Data:
+    def __init__(self,attr_num, path=data_path, target_size=224, mode='gray'):
+        self.gelsight_path = path
+        self.target_size = target_size
+        self.mode = mode
+        self.num_step = 5
+        self.image_channel = 1 if mode=='gray' else 3
+
+
+
+    def S(self,train):
+        S = get_train_attr_label().T if train else get_test_attr_label().T
+        return S
+        # TODO test for the normalize version
+        #return S/[(S[:,i]**2).sum()**0.5 for i in range(S.shape[1])]
+
+    def load_gelsight_data(self):
+        video_list = os.listdir(self.gelsight_path)
+        random.shuffle(video_list)
+        train_videos = []
+        train_labels = []
+        train_attr = []
+        test_videos = []
+        test_labels = []
+        random_index = range(len(video_list))
+        for index in random_index:
+            video_folder = video_list[index]
+            name = video_folder.split('.')[0]
+            class_info = name.split('_')[0].split('F')[1]  # F0001~F0119
+            label = eval(class_info.lstrip('0')) - 1
+            if label >= 100:
+                continue
+            frame_list = os.listdir(os.path.join(self.gelsight_path, video_folder))
+            frame_list.sort()
+            frames = []
+            for frame_name in frame_list:
+                image = cv2.imread(os.path.join(self.gelsight_path, video_folder, frame_name))
+                if self.target_size != 224:
+                    image = cv2.resize(image, (self.target_size, self.target_size))
+                frames.append(image)
+            if label in train_labels:
+                train_videos.append(frames)
+                train_labels.append(label)
+                train_attr.append(class_attribute_matrix[label])
+            elif label in test_labels:
+                test_videos.append(frames)
+                test_labels.append(label)
+        return train_videos, train_labels, test_videos, test_labels
